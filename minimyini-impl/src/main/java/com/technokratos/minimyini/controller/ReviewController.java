510d7a92fd2ab35afcde83f59362373d0346ff3a
Denis Shegaj <shegaj@technokratos.com>
2022-06-07 23:37:13 +0300

    another final commit

--- minimyini-impl/src/main/java/com/technokratos/minimyini/controller/ReviewController.java
+++ minimyini-impl/src/main/java/com/technokratos/minimyini/controller/ReviewController.java
@@ -1,10 +1,15 @@
 package com.technokratos.minimyini.controller;
 
+import com.technokratos.minimyini.dto.ReviewDto;
 import com.technokratos.minimyini.dto.ReviewsDto;
+import com.technokratos.minimyini.security.UserDetailsImpl;
 import com.technokratos.minimyini.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +23,11 @@ public class ReviewController {
     @GetMapping("/{authorId}")
     public ReviewsDto getReviews(@PathVariable Long authorId) {
         return reviewService.getByAuthorId(authorId);
+    }
+
+    @PostMapping("/{hotelId}")
+    public void addReview(@PathVariable Long hotelId, @RequestBody ReviewDto reviewDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        reviewService.save(reviewDto, userDetails.getUser().getId(), hotelId);
     }
 
     @GetMapping("/{hotelId}")
